43642bcf4463858ed8d23e247150749d3b2f092e
Bayode Olorundare <bolorundare@ensembleiq.com>
2018-11-08 23:38:58 -0500

    updated bug fix in app.js

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     store.dispatch(login(user.uid));
-    store.dispatch(startSetExpenses()).then(() => {
+    store.dispatch(startSetOptions()).then(() => {
       renderApp();
       if (history.location.pathname === '/') {
         history.push('/home');
